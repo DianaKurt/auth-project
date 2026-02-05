988f3b5807b0b6274c4fb67c788b94011d7d3e10
Diana <dialeejee18@gmail.com>
2026-02-05 18:15:31 +0500

    new

--- docker/postgres/init/001_schema.sql
+++ docker/postgres/init/001_schema.sql
@@ -2,8 +2,14 @@
 -- This file is executed ONLY on first container creation
 -- Example of users table:
 
-CREATE TABLE USERS (
-    NAME TEXT,
-    SURNAME TEXT,
-    EMAIL TEXT PRIMARY KEY
+CREATE TABLE users (
+  id UUID PRIMARY KEY,
+  name TEXT NOT NULL,
+  email TEXT NOT NULL,
+  password_hash TEXT NOT NULL,
+  status VARCHAR(20) NOT NULL DEFAULT 'unverified',
+  last_login TIMESTAMP
 );
+
+CREATE UNIQUE INDEX idx_users_email_unique
+ON users (email);
